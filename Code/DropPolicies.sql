9433fae1c6ef663f97f8c6c112acd6d967dd6465
walskor <1148905353@qq.com>
2023-10-14 10:35:29 +0800

    User Policies

---
+++ Code/DropPolicies.sql
@@ -0,0 +1,98 @@
+--drop policy if needed
+
+-- ---------------------------------------------------------
+--                  Customer Table
+-- ---------------------------------------------------------
+BEGIN
+    DBMS_RLS.DROP_POLICY (
+        OBJECT_SCHEMA => 'system',
+        OBJECT_NAME => 'Customer',
+        POLICY_NAME => 'GET_CUSTOMER_POLICY'
+    );
+END;
+/
+
+BEGIN
+    DBMS_REDACT.DROP_POLICY (
+        OBJECT_SCHEMA => 'system',
+        OBJECT_NAME => 'Customer',
+        POLICY_NAME => 'mask_Customer'
+    );
+END;
+/
+
+-- ---------------------------------------------------------
+--                  Driver Table
+-- ---------------------------------------------------------
+BEGIN
+    DBMS_RLS.DROP_POLICY (
+        OBJECT_SCHEMA => 'system',
+        OBJECT_NAME => 'Driver',
+        POLICY_NAME => 'GET_DRIVER_POLICY'
+    );
+END;
+/
+
+BEGIN
+    DBMS_REDACT.DROP_POLICY (
+        OBJECT_SCHEMA => 'system',
+        OBJECT_NAME => 'Driver',
+        POLICY_NAME => 'mask_Driver'
+    );
+END;
+/
+
+
+-- ---------------------------------------------------------
+--                  Agent Table
+-- ---------------------------------------------------------
+BEGIN
+    DBMS_RLS.DROP_POLICY (
+        OBJECT_SCHEMA => 'system',
+        OBJECT_NAME => 'Agent',
+        POLICY_NAME => 'GET_AGENT_POLICY'
+    );
+END;
+/
+
+BEGIN
+    DBMS_REDACT.DROP_POLICY (
+        OBJECT_SCHEMA => 'system',
+        OBJECT_NAME => 'Agent',
+        POLICY_NAME => 'mask_Agent'
+    );
+END;
+/
+
+
+-- ---------------------------------------------------------
+--                  Manager Table
+-- ---------------------------------------------------------
+BEGIN
+    DBMS_RLS.DROP_POLICY (
+        OBJECT_SCHEMA => 'system',
+        OBJECT_NAME => 'Manager',
+        POLICY_NAME => 'GET_MANAGER_POLICY'
+    );
+END;
+/
+
+BEGIN
+    DBMS_REDACT.DROP_POLICY (
+        OBJECT_SCHEMA => 'system',
+        OBJECT_NAME => 'Manager',
+        POLICY_NAME => 'mask_Manager'
+    );
+END;
+/
+
+
+-- ---------------------------------------------------------
+--                  Booking Table
+-- ---------------------------------------------------------
+
+
+
+-- ---------------------------------------------------------
+--                  Feedback Table
+-- ---------------------------------------------------------
